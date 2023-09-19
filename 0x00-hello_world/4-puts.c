0e3eab9b0b50fcf850040c81c1eb84a83265763f
Cave-in <klangat844@gmail.com>
2023-09-19 05:36:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main - main entry
+ *return : it will return (0) if successful
+ *puts - this is the output statement
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
