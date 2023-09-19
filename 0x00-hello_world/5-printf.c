406cd5c1d5963969792fc47866112ba30ea6bcc8
Cave-in <klangat844@gmail.com>
2023-09-19 05:50:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * puts - outputs the statement
+ * return : always 0  successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
